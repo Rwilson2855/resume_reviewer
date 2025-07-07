37d60e0dd1677796d10aeabfcd9148727c00bf4b
Rwilson2855 <rwilson2855@gmail.com>
2025-07-07 15:56:45 -0500

    production changes

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def review():
     return jsonify({"review": review_text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
