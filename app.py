0a3ea4344976ff5bda8daa2b111d3cc2d9f95177
Rwilson2855 <rwilson2855@gmail.com>
2025-07-07 15:26:07 -0500

    changes

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def review():
     review_text = review_resume('./input.pdf')
     return jsonify({"review": review_text})
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
